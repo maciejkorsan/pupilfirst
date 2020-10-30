b91705a759dab1ff742ef6ce5731bd5a08ed13f3
stomp <arthurstomp@gmail.com>
2020-10-30 21:59:49 +0100

    feat: More keycloak helper methods

--- app/helpers/keycloak_helper.rb
+++ app/helpers/keycloak_helper.rb
@@ -1,7 +1,5 @@
 module KeycloakHelper
   def self.openid_config
-    site = keycloak_oauth_config['client_options']['site']
-    realm = keycloak_oauth_config['client_options']['realm']
     res = Faraday.get "#{site}/auth/realms/#{realm}/.well-known/openid-configuration"
     if (res.status == 200)
       MultiJson.load(res.body)
@@ -15,10 +13,9 @@ module KeycloakHelper
   end
 
   def self.sign_out(refresh_token)
-    config = keycloak_oauth_config
     params = {
-      'client_id' => config['client_id'],
-      'client_secret' => config['client_secret'],
+      'client_id' => client_id,
+      'client_secret' => client_secret,
       'refresh_token' => refresh_token
     }
     res = Faraday.post(end_session_endpoint, params, { 'Content-Type' => 'application/x-www-form-urlencoded' })
@@ -29,7 +26,88 @@ module KeycloakHelper
     end
   end
 
+  def self.token_endpoint
+    openid_config['token_endpoint']
+  end
+
+  # To manage and view users on Keycloak, the service_account must have 'manage users'
+  # and 'view users' client roles, from client realm-management.
+  def self.service_account
+    params = {
+      'client_id' => client_id,
+      'client_secret' => client_secret,
+      'grant_type' => 'client_credentials',
+    }
+    headers = { 'Content-Type' => 'application/x-www-form-urlencoded' }
+    res = Faraday.post(token_endpoint, params, headers) 
+    if (res.status == 200)
+      MultiJson.load(res.body)
+    else
+      raise 'failed to fetch keycloak service account'
+    end
+  end
+
+  def self.users_uri
+    uri = URI(site)
+    uri.path = "/auth/admin/realms/#{realm}/users"
+    uri
+  end
+
+  def self.users_search(email)
+    uri = users_uri
+    uri.query = "search=#{email}"
+    token = service_account['access_token']
+    res = Faraday.get(uri.to_s, nil, { 'Authorization' => "Bearer #{token}" })
+    if (res.status == 200)
+      MultiJson.load(res.body)
+    else
+      raise 'failed to search for users'
+    end
+  end
+
+  def self.create_user(email, password, first_name, last_name)
+    user_rep = {
+      username: email,
+      email: email,
+      firstName: first_name,
+      lastName: last_name,
+      enabled: true,
+      credentials: [{
+        type: "password",
+        temporary: false,
+        value: password
+      }]
+    }
+    token = service_account['access_token']
+    headers = {
+      'Authorization' => "Bearer #{token}",
+      'Content-Type' => 'application/json'
+    }
+    res = Faraday.post(users_uri.to_s, user_rep.to_json, headers)
+    if (res.status == 201)
+      MultiJson.load(res.body)
+    else
+      raise 'failed to create_user'
+    end
+  end
+
   def self.keycloak_oauth_config
     Devise.omniauth_configs[:keycloak_openid].strategy
+  end
+
+  def self.client_id
+    keycloak_oauth_config['client_id']
+  end
+
+  def self.client_secret
+    keycloak_oauth_config['client_secret']
+  end
+
+  def self.site
+    keycloak_oauth_config['client_options']['site']
+  end
+
+  def self.realm
+    keycloak_oauth_config['client_options']['realm']
   end
 end
